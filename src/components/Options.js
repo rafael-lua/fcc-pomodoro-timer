691a94d0384612856f5fda290092a429cb4d7ece
Rafael <rafael_oliveira@gmx.com>
2021-04-07 18:19:42 -0300

    Update options component and style

--- src/components/Options.js
+++ src/components/Options.js
@@ -6,14 +6,21 @@ function Options(props) {
   return (
     <div className="options-wrapper">
       <div className="options">
-        <div className="icon"><FaArrowCircleUp size="2em" /></div>
-        <p>Value</p>
-        <div className="icon"><FaArrowCircleDown size="2em" /></div>
+        <p className="options-text">Break</p>
+        <div className="options-value">
+          <div className="icon"><FaArrowCircleUp size="2em" /></div>
+          <p className="options-value-text">{props.breakLength}</p>
+          <div className="icon"><FaArrowCircleDown size="2em" /></div>
+        </div>
       </div>
       <div className="options">
-        <div className="icon"><FaArrowCircleUp size="2em" /></div>
-        <p>Value</p>
-        <div className="icon"><FaArrowCircleDown size="2em" /></div>
+        <p className="options-text">Session</p>
+        <div className="options-value">
+          <div className="icon"><FaArrowCircleUp size="2em" /></div>
+          <p className="options-value-text">{props.sessionLength}</p>
+          <div className="icon"><FaArrowCircleDown size="2em" /></div>
+        </div>
+        
       </div>
     </div>
   )
